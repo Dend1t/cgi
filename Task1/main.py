b9716871f7862ee5522e9c29dca9174b7ff728a4
Dend1t <my.dendit.ar@gmail.com>
2026-09-13 02:21:48 +0300

    Task1 Shooting Base

--- Task1/main.py
+++ Task1/main.py
@@ -1,19 +1,30 @@
 from tkinter import *
 
-def on_mouse_motion(event):
-    canvas.moveto('smile', event.x, event.y )
+def crosshair_placement(event):
+    canvas.moveto('crosshair', event.x-radius/2, event.y-radius/2 )
+
+def fire(event):
+    hole_id = canvas.create_oval(event.x - 5, event.y - 5, event.x + 5, event.y + 5, fill='black')
+    display.after(3000, clear_hole, hole_id)
+
+def clear_hole(hole_id):
+    canvas.delete(hole_id)
 
 
 display = Tk()
-display.title("Простой Рисунок")
+display.title("Тир")
 
 canvas = Canvas(display, width = 720, height = 720)
 canvas.pack()
 
 radius = 100
 
-canvas.create_oval(0, 0, radius, radius, fill="yellow", tags='smile')
-display.bind("<Motion>", on_mouse_motion)
+canvas.create_oval(0, 0, radius, radius, outline="red", width=2, tags='crosshair')
+canvas.create_line(radius, radius/2, 0, radius/2, tags='crosshair', width=2)
+canvas.create_line(radius/2, radius, radius/2, 0, tags='crosshair', width=2)
+
+display.bind("<Motion>", crosshair_placement)
+display.bind("<Button-1>", fire)
 
 
 
